0d00dc9d5be5b2401d314de071e6999d8b1d457b
Jason Lingle <jasonlingle@yahoo.com>
2012-08-03 17:35:50 -0500

    Add basic default binding loading system.

--- src/tglng.cxx
+++ src/tglng.cxx
@@ -3,6 +3,7 @@
 #endif
 
 #include <iostream>
+#include <fstream>
 
 #include "interp.hxx"
 #include "common.hxx"
@@ -13,11 +14,22 @@ using namespace tglng;
 int main(int argc, const char*const* argv) {
   wstring out;
   Interpreter interp;
+
+  //Try to read from standard configuration file.
+  //TODO: Change this to something more sensible
+  {
+    wifstream in("rc.default");
+    if (!interp.exec(out, in, Interpreter::ParseModeCommand)) {
+      wcerr << L"Error reading user library." << endl;
+      return EXIT_EXEC_ERROR_IN_USER_LIBRARY;
+    }
+  }
+
   if (interp.exec(out, wcin, Interpreter::ParseModeLiteral)) {
     wcout << out;
     return 0;
   } else {
-    wcerr << "Failed." << endl;
+    wcerr << L"Failed." << endl;
     return EXIT_EXEC_ERROR_IN_INPUT;
   }
 }
