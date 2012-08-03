1f3d02b0f4286ed31dcef2d87dda94d553088cbc
Jason Lingle <jasonlingle@yahoo.com>
2012-08-02 22:28:24 -0500

    Add argument parsing system.

---
+++ src/argument.cxx
@@ -0,0 +1,255 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string>
+#include <cctype>
+#include <cstdlib>
+#include <iostream>
+
+#include "argument.hxx"
+#include "interp.hxx"
+#include "common.hxx"
+
+using namespace std;
+
+namespace tglng {
+  static const wstring sectionTypes(L"<>:|([{$");
+
+  Argument::Argument(Interpreter& interp_, const wstring& text_,
+                     unsigned& offset_, Command*& left_)
+  : interp(interp_), text(text_), offset(offset_), left(left_)
+  { }
+
+  bool Argument::match() {
+    while (offset < text.size() && iswspace(text[offset]))
+      ++offset;
+    return offset < text.size();
+  }
+
+  bool CharArgument::get(wchar_t& out) {
+    out = text[offset++];
+    return true;
+  }
+
+  bool NumericArgument::match() {
+    if (!Argument::match()) return false;
+
+    wchar_t curr = text[offset];
+    return
+      curr == L'0' ||
+      curr == L'1' ||
+      curr == L'2' ||
+      curr == L'3' ||
+      curr == L'4' ||
+      curr == L'5' ||
+      curr == L'6' ||
+      curr == L'7' ||
+      curr == L'8' ||
+      curr == L'9' ||
+      curr == L'+' ||
+      curr == L'-';
+  }
+
+  bool NumericArgument::get(signed& out) {
+    return parseInteger(out, text, offset, &offset);
+  }
+
+  bool CommandArgument::get(Command*& out) {
+    out = NULL;
+    return interp.parse(out, text, offset, Interpreter::ParseModeCommand);
+  }
+
+  bool SectionArgument::match() {
+    if (!Argument::match()) return false;
+
+    return wstring::npos != sectionTypes.find(text[offset]);
+  }
+
+  bool SectionArgument::get(Section& out) {
+    out.left = out.right = NULL;
+
+    switch (text[offset++]) {
+    case L'<':
+      out.left = left;
+      left = NULL;
+      return true;
+
+    case L'>':
+      return interp.parseAll(out.right, text, offset,
+                              Interpreter::ParseModeLiteral) != ParseError;
+
+    case L':':
+      return interp.parse(out.right, text, offset,
+                           Interpreter::ParseModeCommand) == ContinueParsing;
+
+    case L'|':
+      out.left = left;
+      if (interp.parseAll(out.right, text, offset,
+                           Interpreter::ParseModeLiteral) != ParseError) {
+        //Success, may nullify left then return
+        left = NULL;
+        return true;
+      } else {
+        //Since we are failing, out is considered to be meaningless. Thus we
+        //cannot nullify left, since it is still the caller's responsibility.
+        return false;
+      }
+
+    case L'(':
+      return interp.parseAll(out.right, text, offset,
+                              Interpreter::ParseModeCommand) == StopCloseParen;
+
+    case L'[':
+      return interp.parseAll(out.right, text, offset,
+                              Interpreter::ParseModeLiteral) ==StopCloseBracket;
+
+    case L'{': {
+      //ParseModeVerbatim would consume the entire rest of the string, so do
+      //our own conversion.
+      unsigned cnt;
+      unsigned start = offset;
+      for (cnt = 1; cnt && offset < text.size(); ++offset)
+        if (text[offset] == L'{')
+          ++cnt;
+        else if (text[offset] == L'}')
+          --cnt;
+      //After the loop, offset is one past the closing brace
+
+      if (cnt) return false; //Unbalanced brace
+
+      //TODO: out.right = new SelfInsert(text.substr(start,offset-start));
+      cerr << "FATAL: Don't know how to do brace section yet!" << endl;
+      exit(EXIT_THE_SKY_IS_FALLING);
+      return true;
+    }
+
+    case L'$':
+      while (offset < text.size() && iswspace(text[offset])) ++offset;
+
+      if (offset >= text.size()) return false;
+
+      //TODO: out.right = new RegisterRead(text[offset++]);
+      cerr << "FATAL: Don't know how to do register section yet!" << endl;
+      exit(EXIT_THE_SKY_IS_FALLING);
+      return true;
+    }
+
+    wcerr << L"FATAL: Unandled section type: " << text[offset-1] << endl;
+    exit(EXIT_THE_SKY_IS_FALLING);
+  }
+
+  SentinelStringArgument::SentinelStringArgument(Interpreter& interp,
+                                                 const wstring& text,
+                                                 unsigned& offset,
+                                                 Command*& left,
+                                                 wchar_t sent)
+  : Argument(interp, text, offset, left),
+    sentinel(sent)
+  { }
+
+  bool SentinelStringArgument::match() {
+    if (!Argument::match()) return false;
+
+    if (sentinel == text[offset])
+      //Forbid empty string
+      return false;
+
+    for (unsigned i = offset+1; i < text.size(); ++i)
+      if (text[i] == sentinel)
+        return true;
+
+    return false;
+  }
+
+  bool SentinelStringArgument::get(wstring& dst) {
+    unsigned start = offset;
+    do {
+      ++offset;
+    } while (text[offset] != sentinel);
+
+    dst = text.substr(start, offset-start);
+    //Past the terminating character
+    ++offset;
+    return true;
+  }
+
+  bool AlnumStringArgument::match() {
+    if (!Argument::match()) return false;
+    //Use the char version of isalnum, since this is only used by commands
+    //which assign special meaning to each character.
+    //ANSI C does not define what happens if the character is outside of
+    //0..127 (and indeed the MS C library crashes since it uses an array), so
+    //do this check ourselves.
+    //(Cast to char for clarity.)
+    wchar_t curr = text[offset];
+    return curr >= 0 && curr <= 127 && isalnum((char)curr);
+  }
+
+  bool AlnumStringArgument::get(wstring& dst) {
+    unsigned start = offset;
+    do {
+      ++offset;
+    //See comments in ::match() regarding this test.
+    } while (text[offset] >= 0 && text[offset] <= 127 &&
+             isalnum((char)text[offset]));
+
+    dst = text.substr(start, offset-start);
+    return true;
+  }
+
+  bool NonSectionStringArgument::match() {
+    if (!Argument::match()) return false;
+
+    return wstring::npos == sectionTypes.find(text[offset]);
+  }
+
+  bool NonSectionStringArgument::get(wstring& dst) {
+    unsigned start = offset;
+    do {
+      ++offset;
+    } while (wstring::npos == sectionTypes.find(text[offset]));
+
+    dst = text.substr(start, offset-start);
+    return true;
+  }
+
+  bool ArithmeticArgument::get(Command*& dst) {
+    wchar_t fst = text[offset];
+    if (fst >= L'0' && fst <= L'9') {
+      unsigned start = offset;
+      signed discard;
+      if (!parseInteger(discard, text, start, &offset))
+        //Invalid integer
+        return false;
+      //Valid integer, but keep the original string representation in case it
+      //matters
+      //TODO: dst = new SelfInsert(text.substr(start, offset-start));
+      cerr << "FATAL: Don't know how to wrap numeric literal arithmetic yet."
+           << endl;
+      exit(EXIT_THE_SKY_IS_FALLING);
+      return true;
+    } else {
+      return interp.parse(dst, text, offset, Interpreter::ParseModeCommand);
+    }
+  }
+
+  ExactCharacterArgument::ExactCharacterArgument(Interpreter& interp,
+                                                 const wstring& text,
+                                                 unsigned& offset,
+                                                 Command*& left,
+                                                 wchar_t expect_)
+  : Argument(interp, text, offset, left),
+    expect(expect_)
+  { }
+
+  bool ExactCharacterArgument::match() {
+    return Argument::match() && text[offset] == expect;
+  }
+
+  bool ExactCharacterArgument::get(bool& dst) {
+    dst = true;
+    ++offset;
+    return true;
+  }
+}
---
+++ src/argument.hxx
@@ -0,0 +1,328 @@
+#ifndef ARGUMENT_HXX_
+#define ARGUMENT_HXX_
+
+#include <string>
+
+namespace tglng {
+  class Command;
+  class Interpreter;
+
+  /**
+   * Encapsulates the data and basic semantics for argument extraction.
+   */
+  class Argument {
+  protected:
+    Interpreter& interp;
+    const std::wstring& text;
+    unsigned& offset;
+    Command*& left;
+
+    Argument(Interpreter&, const std::wstring&, unsigned&, Command*&);
+
+  public:
+    /**
+     * Tests whether the string matches.
+     *
+     * Default advances past all whitespace and then returns whether a
+     * non-whitespace character was found.
+     */
+    bool match();
+  };
+
+#define CTOR(clazz) \
+  clazz##Argument(Interpreter& i, const std::wstring& s, \
+                  unsigned& u, Command*& c)              \
+  : Argument(i,s,u,c) {}
+
+  /**
+   * Extracts a single non-whitespace character as an argument.
+   */
+  class CharArgument: public Argument {
+  public:
+    CTOR(Char)
+    typedef wchar_t get_t;
+    bool get(get_t&);
+  };
+
+  /**
+   * Extracts a numeric literal as an argument.
+   */
+  class NumericArgument: public Argument {
+  public:
+    CTOR(Numeric)
+    typedef signed get_t;
+    bool get(get_t&);
+    bool match();
+  };
+
+  /**
+   * Extracts and parses a single Command as an argument.
+   */
+  class CommandArgument: public Argument {
+  public:
+    CTOR(Command)
+    typedef Command* get_t;
+    bool get(get_t&);
+  };
+
+  /**
+   * Defines the type of a section arguement.
+   */
+  struct Section {
+    Command* left, * right;
+  };
+  /**
+   * Extracts a Section argument type from input.
+   */
+  class SectionArgument: public Argument {
+  public:
+    CTOR(Section)
+    typedef Section get_t;
+    bool get(get_t&);
+    bool match();
+  };
+
+  /**
+   * Extracts a string from point to the given sentinal. Does not match if the
+   * sentinel is not found.
+   */
+  class SentinelStringArgument: public Argument {
+    wchar_t sentinel;
+
+  public:
+    SentinelStringArgument(Interpreter&, const std::wstring&, unsigned&,
+                           Command*&, wchar_t);
+    typedef std::wstring get_t;
+    bool get(get_t&);
+    bool match();
+  };
+
+  /**
+   * Extracts a non-empty string of alphanumeric characters starting at point
+   * and continuing to (but not including) the first non-alphanumeric character
+   * or end of input.
+   */
+  class AlnumStringArgument: public Argument {
+  public:
+    CTOR(AlnumString)
+    typedef std::wstring get_t;
+    bool get(get_t&);
+    bool match();
+  };
+
+  /**
+   * Extracts a non-empty string of characters starting at point and continuing
+   * to (but not including) the first whitespace character, the first character
+   * which is a valid section specifier, or end of input.
+   */
+  class NonSectionStringArgument: public Argument {
+  public:
+    CTOR(NonSectionString)
+    typedef std::wstring get_t;
+    bool get(get_t&);
+    bool match();
+  };
+
+  /**
+   * Extracts either a single parsed command, or a literal number which is
+   * wrapped in a command and returned.
+   */
+  class ArithmeticArgument: public Argument {
+  public:
+    CTOR(Arithmetic)
+    typedef Command* get_t;
+    bool get(get_t&);
+  };
+
+  /**
+   * Matches a single character. If matched, extracts true to a boolean.
+   */
+  class ExactCharacterArgument: public Argument {
+    wchar_t expect;
+
+  public:
+    ExactCharacterArgument(Interpreter&, const std::wstring&, unsigned&,
+                           Command*&, wchar_t);
+    typedef bool get_t;
+    bool get(get_t&);
+    bool match();
+  };
+#undef CTOR
+
+  /**
+   * Wraps the Argument classes to store the destination reference within.
+   */
+  template<typename T>
+  class ArgumentExtractor {
+  protected:
+    T it;
+    typename T::get_t& dst;
+
+  public:
+    ArgumentExtractor(const T& t, typename T::get_t& d)
+    : it(t), dst(d)
+    { }
+
+    bool match() { return it.match(); }
+    bool get() { return it.get(dst); }
+  };
+
+  /**
+   * Chains two ArgumentExtractor-compatible classes into one.
+   */
+  template<typename First, typename Rest>
+  class ArgumentSequence {
+    First first;
+    Rest rest;
+
+  public:
+    ArgumentSequence(const First& f, const Rest& r)
+    : first(f), rest(r) {}
+
+    bool match() { return first.match(); }
+    bool get() { return first.get() && rest.match() && rest.get(); }
+  };
+
+  /**
+   * Tries to match with one ArgumentExtractor-compatible class, then falls
+   * back on the other.
+   */
+  template<typename First, typename Rest>
+  class ArgumentOptions {
+    First first;
+    Rest rest;
+
+  public:
+    ArgumentOptions(const First& f, const Rest& r)
+    : first(f), rest(r) {}
+
+    bool match() { return first.match() || rest.match(); }
+    bool get() { return first.match()? first.get() : rest.get(); }
+  };
+
+  /**
+   * Wraps an ArgumentExtractor-compatible class to make it optional.
+   */
+  template<typename T>
+  class OptionalArgument {
+    T it;
+
+  public:
+    OptionalArgument(const T& t) : it(t) {}
+
+    bool match() { return true; }
+    bool get() { return !it.match() || it.get(); }
+  };
+
+  /**
+   * Adds operators to ArgumentExtractor which give a nice syntax sugar for
+   * compound arguments:
+   *
+   *   arg1, arg2  -> ArgumentSequence<arg1,arg2>
+   *   arg1 | arg2 -> ArgumentOptions<arg1,arg2>
+   *   -arg1       -> OptionalArgument<arg1>
+   */
+  template<typename Contained>
+  class ArgumentSyntaxSugar {
+    Contained it;
+
+  public:
+    ArgumentSyntaxSugar(const Contained& t)
+    : it(t) {}
+
+    bool match() { return it.match(); }
+    bool get() { return it.get(); }
+
+    template<typename Next>
+    ArgumentSyntaxSugar<ArgumentSequence<Contained, Next> >
+    operator,(const Next& next) const {
+      return ArgumentSyntaxSugar<ArgumentSequence<Contained, Next> >(
+        ArgumentSequence<Contained,Next>(it, next));
+    }
+
+    template<typename Other>
+    ArgumentSyntaxSugar<ArgumentOptions<Contained, Other> >
+    operator|(const Other& other) const {
+      return ArgumentSyntaxSugar<ArgumentOptions<Contained, Other> >(
+        ArgumentOptions<Contained,Other>(it, other));
+    }
+
+    ArgumentSyntaxSugar<OptionalArgument<Contained> >
+    operator-() const {
+      return ArgumentSyntaxSugar<OptionalArgument<Contained> >(
+        OptionalArgument<Contained>(it));
+    }
+  };
+
+  /**
+   * Simplifies the construction and testing of argument extractors.
+   */
+  class ArgumentParser {
+    Interpreter& interp;
+    const std::wstring& text;
+    unsigned& offset;
+    Command*& left;
+
+  public:
+    /**
+     * Creates an ArgumentParser with the given arguments required for all
+     * Arguments.
+     */
+    ArgumentParser(Interpreter&, const std::wstring&,
+                   unsigned&, Command*& left);
+
+    ///Returns a wrapped CharArgument which discards its character.
+    ArgumentSyntaxSugar<ArgumentExtractor<CharArgument> > h();
+    ///Returns a wrapped CharArgument storing the character into the given
+    ///destination
+    ArgumentSyntaxSugar<ArgumentExtractor<CharArgument> > h(wchar_t&);
+    ///Returns a wrapped NumericArgument storing the integer into the given
+    ///destination.
+    ArgumentSyntaxSugar<ArgumentExtractor<NumericArgument> > n(signed&);
+    ///Returns a wrapped ArithmeticArgument storing the Command* into the given
+    ///destination.
+    ArgumentSyntaxSugar<ArgumentExtractor<ArithmeticArgument> > a(Command*&);
+    ///Returns a wrapped SectionArgument storing the Section into the given
+    ///destination.
+    ArgumentSyntaxSugar<ArgumentExtractor<SectionArgument> > s(Section&);
+    ///Returns a wrapped SentinelStringArgument storing the string into the
+    ///given destination and terminating on the given character.
+    ArgumentSyntaxSugar<ArgumentExtractor<SentinelStringArgument> >
+    to(std::wstring&, wchar_t);
+    ///Returns a wrapped AlnumStringArgument storing the string into the given
+    ///destination.
+    ArgumentSyntaxSugar<ArgumentExtractor<AlnumStringArgument> >
+    an(std::wstring&);
+    ///Returns a wrapped NonSectionStringArgument storing the string into the
+    ///given destination.
+    ArgumentSyntaxSugar<ArgumentExtractor<NonSectionStringArgument> >
+    ns(std::wstring&);
+    ///Returns an ExactCharacterArgument matching the given character. The
+    ///extracted boolean is dropped.
+    ArgumentSyntaxSugar<ArgumentExtractor<ExactCharacterArgument> > x(wchar_t);
+    ///Returns an ExactCharacterArgument storing the boolean into the given
+    ///location and matching the given character.
+    ArgumentSyntaxSugar<ArgumentExtractor<ExactCharacterArgument> >
+    x(bool&, wchar_t);
+    ///Returns a CommandArgument storing the command into the given destination.
+    ArgumentSyntaxSugar<ArgumentExtractor<CommandArgument> > c(Command*&);
+
+    /**
+     * Tries to match the given compound argument agains the input. If matching
+     * and parsing is successful, returns true. Otherwise, prints a diagnostic
+     * and returns false.
+     */
+    template<typename T>
+    bool operator[](T& t) {
+      if (!t.match() || !t.get()) {
+        diagnostic();
+        return false;
+      } else return true;
+    }
+
+  private:
+    void diagnostic();
+  };
+}
+
+#endif /* ARGUMENT_HXX_ */
