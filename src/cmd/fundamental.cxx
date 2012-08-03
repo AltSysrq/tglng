ad0e1807dff9524b1dedf0c39b8feb2d1fc2e4bb
Jason Lingle <jasonlingle@yahoo.com>
2012-08-03 14:23:34 -0500

    Add long-command.

--- src/cmd/fundamental.cxx
+++ src/cmd/fundamental.cxx
@@ -8,6 +8,7 @@
 #include "fundamental.hxx"
 #include "../command.hxx"
 #include "../interp.hxx"
+#include "../argument.hxx"
 #include "../common.hxx"
 
 using namespace std;
@@ -34,4 +35,32 @@ namespace tglng {
     out = new SelfInsertCommand(out, text[offset++]);
     return ContinueParsing;
   }
+
+  static GlobalBinding<SelfInsertParser> _selfInsertParser(L"self-insert");
+
+  class LongCommandParser: public CommandParser {
+  public:
+    virtual ParseResult parse(Interpreter& interp, Command*& out,
+                              const std::wstring& text,
+                              unsigned& offset) {
+      wstring name;
+      ArgumentParser a(interp, text, offset, out);
+      if (!a[a.h(), a.to(name, L'#')]) return ParseError;
+
+      //Move back to the closing hash, since that is the effective character
+      //for the command.
+      --offset;
+
+      map<wstring,CommandParser*>::const_iterator it =
+        interp.commandsL.find(name);
+      if (it == interp.commandsL.end()) {
+        interp.error(wstring(L"Unknown command: ") + name, text, offset);
+        return ParseError;
+      }
+
+      return it->second->parse(interp, out, text, offset);
+    }
+  };
+
+  static GlobalBinding<LongCommandParser> _longCommandParser(L"long-command");
 }
