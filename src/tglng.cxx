b47fa955de10fc680af3a001b85fa9648ac65ac2
Jason Lingle <jasonlingle@yahoo.com>
2012-08-03 14:28:21 -0500

    Main function now does something.

--- src/tglng.cxx
+++ src/tglng.cxx
@@ -1,8 +1,23 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <iostream>
 
+#include "interp.hxx"
+#include "common.hxx"
+
 using namespace std;
+using namespace tglng;
 
 int main(int argc, const char*const* argv) {
-  cout << "Hello, world!" << endl;
-  return 0;
+  wstring out;
+  Interpreter interp;
+  if (interp.exec(out, wcin, Interpreter::ParseModeLiteral)) {
+    wcout << out;
+    return 0;
+  } else {
+    wcerr << "Failed." << endl;
+    return EXIT_EXEC_ERROR_IN_INPUT;
+  }
 }
