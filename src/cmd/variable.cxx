71037ff0362b1d63e918917d7fc5c040111c018b
Jason Lingle <jasonlingle@yahoo.com>
2012-08-19 13:02:55 -0600

    Add functional-like pseudo-let command.

---
+++ src/cmd/variable.cxx
@@ -0,0 +1,172 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string>
+#include <memory>
+
+#include "../interp.hxx"
+#include "../command.hxx"
+#include "../argument.hxx"
+#include "../common.hxx"
+
+using namespace std;
+
+namespace tglng {
+  //Stores a reference-counted, shared string value.
+  struct VariableValue {
+    wstring value;
+    unsigned refcount;
+  };
+
+  //Automatically handles refcounting on VariableValue
+  class Variable {
+    VariableValue*const value;
+
+  public:
+    Variable()
+    : value (new VariableValue)
+    {
+      value->refcount=1;
+    }
+
+    Variable(VariableValue* v)
+    : value(v)
+    {
+      ++value->refcount;
+    }
+
+    Variable(const wstring& str)
+    : value(new VariableValue)
+    {
+      value->value = str;
+      value->refcount = 1;
+    }
+
+    Variable(const Variable& that)
+    : value(that.value)
+    {
+      ++value->refcount;
+    }
+
+    ~Variable() {
+      if (!--value->refcount)
+        delete value;
+    }
+
+    wstring& get() {
+      return value->value;
+    }
+  };
+
+  class VariableSet: public Command {
+  protected:
+    Variable var;
+
+  private:
+    auto_ptr<Command> value;
+
+  public:
+    VariableSet(Command* left,
+                const Variable& var_,
+                auto_ptr<Command>& value_)
+    : Command(left), var(var_), value(value_)
+    { }
+
+    virtual bool exec(wstring& dst, Interpreter& interp) {
+      return interp.exec(var.get(), value.get());
+    }
+  };
+
+  class VariableLet: public VariableSet {
+    auto_ptr<Command> body;
+
+  public:
+    VariableLet(Command* left,
+                const Variable& var,
+                auto_ptr<Command>& value,
+                auto_ptr<Command>& body_)
+    : VariableSet(left, var, value), body(body_)
+    { }
+
+    virtual bool exec(wstring& dst, Interpreter& interp) {
+      wstring old(var.get());
+      if (!VariableSet::exec(dst, interp))
+        return false;
+
+      bool result = interp.exec(dst, body.get());
+      var.get() = old;
+      return result;
+    }
+  };
+
+  class VariableGet: public Command {
+    Variable var;
+
+  public:
+    VariableGet(Command* left, const Variable& var_)
+    : Command(left), var(var_) {}
+
+    virtual bool exec(wstring& dst, Interpreter& interp) {
+      dst = var.get();
+      return true;
+    }
+  };
+
+  class VariableGetParser: public CommandParser {
+    Variable var;
+
+  public:
+    VariableGetParser(const Variable& var_)
+    : var(var_) {}
+
+    virtual ParseResult parse(Interpreter& interp, Command*& out,
+                              const wstring& text, unsigned& offset) {
+      ++offset; //Past "command char"
+      out = new VariableGet(out, var);
+      return ContinueParsing;
+    }
+  };
+
+  class VariableLetParser: public CommandParser {
+  public:
+    virtual ParseResult parse(Interpreter& interp, Command*& out,
+                              const wstring& text, unsigned& offset) {
+      wstring name;
+      auto_ptr<Command> value, body;
+      ArgumentParser a(interp, text, offset, out);
+      if (!a[a.h(), a.to(name, L'#'), a.x(L'='), a.a(value)])
+        return ParseError;
+
+      Variable var;
+
+      //Create a temporary parser for the variable; first, keep whatever had
+      //that command before.
+      CommandParser* oldParser = interp.commandsL[name];
+      CommandParser* newParser = new VariableGetParser(var);
+      newParser->isTemporary = true;
+      interp.commandsL[name] = newParser;
+
+      //Get the body of the let
+      Command* rawBody = NULL;
+      ParseResult result = interp.parseAll(rawBody, text, offset,
+                                           Interpreter::ParseModeCommand);
+      body.reset(rawBody);
+
+      //Restore the old command
+      if (oldParser)
+        interp.commandsL[name] = oldParser;
+      else
+        interp.commandsL.erase(name);
+      delete newParser;
+
+      //Create the let command if all OK
+      if (result != ParseError)
+        out = new VariableLet(out, var, value, body);
+
+      return result;
+    }
+  };
+
+  static GlobalBinding<VariableLetParser> _let(L"let");
+}
