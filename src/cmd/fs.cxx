43d599a38e7ea149657fb5f2d224dda982a5c2e3
Jason Lingle <jasonlingle@yahoo.com>
2012-08-22 20:05:42 -0600

    Add read and read-binary, and fix leak in ls.

--- src/cmd/fs.cxx
+++ src/cmd/fs.cxx
@@ -8,6 +8,7 @@
 #include <clocale>
 #include <string>
 #include <cstring>
+#include <cerrno>
 
 /* Use the POSIX glob(3) function if available. On a native Windows port,
  * you'll need to make your own glob(3) implementation and define HAVE_GLOB, or
@@ -87,6 +88,8 @@ namespace tglng {
         //OK, add to the list
         list::lappend(*out, wstring(&str[0], wend));
       }
+
+      globfree(&results);
     }
 
     return true;
@@ -94,4 +97,85 @@ namespace tglng {
 
   static GlobalBinding<TFunctionParser<1,1,fs_ls> > _ls(L"ls");
 #endif /* HAVE_GLOB */
+
+  /**
+   * Converts the given wstring to a char*, stored within dst. The actual size
+   * of dst is not defined; rather, the only guarantee is that &dst[0] is a
+   * valid NTBS.
+   *
+   * If successful, returns true; otherwise, prints a diagnostic and returns
+   * false.
+   */
+  static bool wstrtofn(vector<char>& dst, const wstring& src) {
+    typedef codecvt<wchar_t, char, mbstate_t> converter_t;
+    locale theLocale("");
+    const converter_t& converter =
+      use_facet<converter_t>(theLocale);
+
+    dst.resize(src.size() * converter.max_length() + 1);
+    mbstate_t state;
+    const wchar_t* from_next;
+    char* end;
+    converter_t::result result =
+      converter.out(state, src.data(), src.data() + src.size(),
+                    from_next, &dst[0], &dst[0] + dst.size() - 1,
+                    end);
+    if (result != converter_t::ok) {
+      wcerr << L"Could not convert filename to narrow string: "
+            << src << endl;
+      return false;
+    }
+
+    //NUL-terminate
+    *end = 0;
+
+    return true;
+  }
+
+  static void blitstr(wstring& dst, const wstring& src) {
+    dst = src;
+  }
+  static void blitstr(wstring& dst, const string& src) {
+    dst.resize(src.size());
+    for (unsigned i = 0; i < src.size(); ++i)
+      dst[i] = (wchar_t)(unsigned char)src[i];
+  }
+  static void blitstr(string& dst, const wstring& src) {
+    dst.resize(src.size());
+    for (unsigned i = 0; i < src.size(); ++i)
+      dst[i] = (char)src[i];
+  }
+
+  template<typename Ifstream, typename String, bool Text>
+  bool fs_read(wstring* out, const wstring* in,
+               Interpreter& interp, unsigned) {
+    vector<char> fname;
+    if (!wstrtofn(fname, in[0]))
+      return false;
+    Ifstream input(&fname[0], Text? ios::in : ios::in | ios::binary);
+    if (!input) {
+      cerr << strerror(errno) << endl;
+      out[0].clear();
+      out[1] = L"0";
+      return true;
+    }
+
+    String str;
+    static String eof((size_t)1, 4);
+    getline(input, str, eof[0]);
+    blitstr(out[0], str);
+
+    if (!input.fail() && !input.eof())
+      out[1] = L"1";
+    else
+      out[1] = L"0";
+
+    return true;
+  }
+
+  static GlobalBinding<TFunctionParser<2,1,fs_read<wifstream, wstring, true> > >
+  _read(L"read");
+  static GlobalBinding<TFunctionParser<
+                         2,1,fs_read<ifstream, string, false> > >
+  _readBinary(L"read-binary");
 }
